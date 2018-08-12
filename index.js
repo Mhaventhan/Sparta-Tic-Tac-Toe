0d44bea5e9aabe4451340700c0e18356822438ad
Mhaventhan Arunthavalingam <mhaventhan@gmail.com>
2018-08-12 16:36:37 +0100

    new variables declared

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 $(function(){
 // selecting the square element from index.html to change
-
-var playerTurns = 0,
+var
+gameReset = $('#replay'),
 player = 1,
-winMatch = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[0,4,8],[2,5,8],[2,4,6]];
+playerPosition= [0,0,0,0,0,0,0,0];
+
+
 
   $('.square').on('click', changeTo)
   function changeTo() {
